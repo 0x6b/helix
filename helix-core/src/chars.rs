314b027f16786853b7f7afa86a680374a8d09ad1
kaoru <k@warpnine.io>
2024-01-13 11:57:56 +0900

    Update Japanese Hiragana and Kanji range based on The Unicode Standard, Version 15.1

--- helix-core/src/chars.rs
+++ helix-core/src/chars.rs
@@ -36,24 +36,36 @@ pub fn categorize_char(ch: char) -> CharCategory {
 }
 
 // Determine whether a character is a hiragana character.
-// Reference: https://www.unicode.org/charts/PDF/U3040.pdf
 #[inline]
 pub fn char_is_hiragana(ch: char) -> bool {
-    ('\u{3040}'..='\u{309f}').contains(&ch)
+    ('\u{3041}'..='\u{3096}').contains(&ch) || ('\u{3099}'..='\u{309F}').contains(&ch) // Hiragana: https://www.unicode.org/charts/PDF/U3040.pdf
+        || ('\u{1B100}'..='\u{1B12F}').contains(&ch) // Kana Extended-A: https://www.unicode.org/charts/PDF/U1B100.pdf
+        || ('\u{1AFF0}'..='\u{1AFFF}').contains(&ch) // Kana Extended-B: https://www.unicode.org/charts/PDF/U1AFF0.pdf
+        || ('\u{1B000}'..='\u{1B0FF}').contains(&ch) // Kana Supplement: https://www.unicode.org/charts/PDF/U1B000.pdf
+        || ('\u{1B130}'..='\u{1B16F}').contains(&ch) // Small Kana Extension: https://www.unicode.org/charts/PDF/U1B130.pdf
 }
 
 // Determine whether a character is a katakana character.
-// Reference: https://www.unicode.org/charts/PDF/U30A0.pdf
 #[inline]
 pub fn char_is_katakana(ch: char) -> bool {
-    ('\u{30a0}'..='\u{30ff}').contains(&ch)
+    ('\u{30A0}'..='\u{30FF}').contains(&ch) // Katakana: https://www.unicode.org/charts/PDF/U30A0.pdf
 }
 
 // Determine whether a character is a kanji, or CJK Unified Ideographs, character.
-// Reference: https://www.unicode.org/charts/PDF/U4E00.pdf
 #[inline]
 pub fn char_is_kanji(ch: char) -> bool {
-    ('\u{4e00}'..='\u{9fbf}').contains(&ch)
+    ('\u{4E00}'..='\u{9FFF}').contains(&ch) // CJK Unified Ideographs: https://www.unicode.org/charts/PDF/U4E00.pdf
+        || ('\u{3400}'..='\u{4DBF}').contains(&ch) // CJK Unified Ideographs Extension A: https://www.unicode.org/charts/PDF/U3400.pdf
+        || ('\u{20000}'..='\u{2A6DF}').contains(&ch) // CJK Unified Ideographs Extension B: https://www.unicode.org/charts/PDF/U20000.pdf
+        || ('\u{2A700}'..='\u{2B739}').contains(&ch) // CJK Unified Ideographs Extension C: https://www.unicode.org/charts/PDF/U2A700.pdf
+        || ('\u{2B740}'..='\u{2B81D}').contains(&ch) // CJK Unified Ideographs Extension D: https://www.unicode.org/charts/PDF/U2B740.pdf
+        || ('\u{2B820}'..='\u{2CEA1}').contains(&ch) // CJK Unified Ideographs Extension E: https://www.unicode.org/charts/PDF/U2B820.pdf
+        || ('\u{2CEB0}'..='\u{2EBE0}').contains(&ch) // CJK Unified Ideographs Extension F: https://www.unicode.org/charts/PDF/U2CEB0.pdf
+        || ('\u{30000}'..='\u{3134A}').contains(&ch) // CJK Unified Ideographs Extension G: https://www.unicode.org/charts/PDF/U30000.pdf
+        || ('\u{31350}'..='\u{323AF}').contains(&ch) // CJK Unified Ideographs Extension H: https://www.unicode.org/charts/PDF/U31350.pdf
+        || ('\u{2EBF0}'..='\u{2EE5D}').contains(&ch) // CJK Unified Ideographs Extension H: https://www.unicode.org/charts/PDF/U2EBF0.pdf
+        || ('\u{F900}'..='\u{FAFF}').contains(&ch) // CJK Compatibility Ideographs: https://www.unicode.org/charts/PDF/UF900.pdf
+        || ('\u{2F800}'..='\u{2FA1F}').contains(&ch) // CJK Compatibility Ideographs Supplement: https://www.unicode.org/charts/PDF/U2F800.pdf
 }
 
 /// Determine whether a character is a line ending.
