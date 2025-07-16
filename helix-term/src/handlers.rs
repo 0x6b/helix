0ffe510be9ec1015675a7a99f8570ddbd560a0c5
Anthony Rubick <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2025-07-15 17:43:18 -0700

    feat: automatic reload when file changes externally
    Implements the idea raised by @porridgewithraisins in this comment:
    https://github.com/helix-editor/helix/issues/1125#issuecomment-3073782827
    
    The implementation was modeled after the implementation of the autosave feature.
    
    Here are some example snippets of configuration for this feature:
    
    - disable auto-reloading (default)
    ```toml
    [editor]
    auto-reload = false
    ```
    or
    ```toml
    [editor.auto-reload]
    focus-gained = false
    ```
    - auto-reload on focus
    ```toml
    [editor]
    auto-reload = true
    ```
    or
    ```toml
    [editor.auto-reload]
    focus-gained = true
    ```
    - auto-reload at some periodically at time interval (5 seconds in this example)
    ```toml
    [editor.auto-reload]
    periodic.enable = true
    periodic.interval = 5000
    ```
    - of course, you could have it reload on focus and at an interval too:
    ```toml
    [editor.auto-reload]
    focus-gained = true
    periodic.enable = true
    periodic.interval = 5000
    ```

--- helix-term/src/handlers.rs
+++ helix-term/src/handlers.rs
@@ -6,6 +6,7 @@ use helix_event::AsyncHook;
 
 use crate::config::Config;
 use crate::events;
+use crate::handlers::auto_reload::AutoReloadHandler;
 use crate::handlers::auto_save::AutoSaveHandler;
 use crate::handlers::diagnostics::PullDiagnosticsHandler;
 use crate::handlers::signature_help::SignatureHelpHandler;
@@ -15,6 +16,7 @@ pub use helix_view::handlers::{word_index, Handlers};
 use self::document_colors::DocumentColorsHandler;
 use self::document_links::DocumentLinksHandler;
 
+mod auto_reload;
 mod auto_save;
 mod code_action_hint;
 pub mod completion;
@@ -34,6 +36,7 @@ pub fn setup(config: Arc<ArcSwap<Config>>) -> Handlers {
     let signature_hints = SignatureHelpHandler::new().spawn();
     let auto_save = AutoSaveHandler::new().spawn();
     let code_action_hint = code_action_hint::Handler::default().spawn();
+    let auto_read = AutoReloadHandler::new().spawn();
     let document_colors = DocumentColorsHandler::default().spawn();
     let document_links = DocumentLinksHandler::default().spawn();
     let word_index = word_index::Handler::spawn();
@@ -44,6 +47,7 @@ pub fn setup(config: Arc<ArcSwap<Config>>) -> Handlers {
         completions: helix_view::handlers::completion::CompletionHandler::new(event_tx),
         signature_hints,
         auto_save,
+        auto_reload: auto_read,
         document_colors,
         document_links,
         word_index,
@@ -58,6 +62,7 @@ pub fn setup(config: Arc<ArcSwap<Config>>) -> Handlers {
     document_highlight::register_hooks(&handlers);
     code_action_hint::register_hooks(&handlers);
     auto_save::register_hooks(&handlers);
+    auto_reload::register_hooks(&handlers);
     diagnostics::register_hooks(&handlers);
     snippet::register_hooks(&handlers);
     document_colors::register_hooks(&handlers);
